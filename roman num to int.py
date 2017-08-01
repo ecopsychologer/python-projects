58bbe4988e5fba26cbba3e1939adac331d9c79b1
Jack Schoen <schoenjack98@gmail.com>
2017-08-01 13:35:27 -0400

    getting there
    can now tell if input is an int

--- roman num to int.py
+++ roman num to int.py
@@ -1,13 +1,16 @@
 num = input("Enter a roman numeral or an integer: ")
+ch1 = num[0]
+roman1 = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]
 
 
-def firstChar(num1):
-    ch1 = num1.str(0)
-    if ((ch1 == '0') | (ch1 == '1') | (ch1 == '2') | (ch1 == '3') | (ch1 == '4') | (ch1 == '5') | (ch1 == '6') | (ch1 == '7') | (ch1 == '8') | (ch1 == '9')):
-        num = int(num, 10)
+def romify(num1):
+    num2 = num1/10 - 1
+    romanNum
+if int(num) > 4999:
+    finalNum = "ERROR: NUMBER TOO HIGH"
+if ch1 == '0' | ch1 == '1' | ch1 == '2' | ch1 == '3' | ch1 == '4' | ch1 == '5' | ch1 == '6' | ch1 == '7' | ch1 == '8' | ch1 == '9':
+    finalNum = int(num)
+else:
+    finalNum = ""
 
-firstChar(num)
-
-#if ((ch1 == '0') | (ch1 == '1') | (ch1 == '2') | (ch1 == '3') | (ch1 == '4') | (ch1 == '5') | (ch1 == '6') | (ch1 == '7') | (ch1 == '8') | (ch1 == '9')):
-#    num = int(num, 10)
-print(type(num))
+print(finalNum)
